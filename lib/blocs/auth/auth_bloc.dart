4c86fcdb63b1f4fa56f7ea66527738c4bb8f8fee
TreeAngel <122539511+TreeAngel@users.noreply.github.com>
2024-11-04 17:10:17 +0700

    Minor bug fix

--- lib/blocs/auth/auth_bloc.dart
+++ lib/blocs/auth/auth_bloc.dart
@@ -104,8 +104,8 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     if (signOutResponse is ApiErrorResponseModel) {
       String? message =
           signOutResponse.error?.message ?? signOutResponse.error?.error;
-      if (message.toString().trim().toLowerCase().contains('invalid') ||
-          message.toString().trim().toLowerCase().contains('unauthorized')) {
+      if (!message.toString().trim().toLowerCase().contains('invalid') ||
+          !message.toString().trim().toLowerCase().contains('unauthorized')) {
         message = signOutResponse.toString();
       }
       emit(AuthError(message: message.toString()));
@@ -173,12 +173,8 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       } else if (signInResponse is ApiErrorResponseModel) {
         String? message =
             signInResponse.error?.error ?? signInResponse.error?.message;
-<<<<<<< HEAD
         if (message.toString().trim().toLowerCase().contains('invalid') ||
             message.toString().trim().toLowerCase().contains('unauthorized')) {
-=======
-        if (message.toString().trim().toLowerCase().contains('invalid') || message.toString().trim().toLowerCase().contains('unauthorized')) {
->>>>>>> 295ec5d4f88909e7c0e9d6c46cec6ba2a23aa7a7
           message = 'Username atau password salah';
         }
         emit(AuthError(message: message.toString()));
